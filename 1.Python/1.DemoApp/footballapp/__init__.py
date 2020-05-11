d3ff200213c77d0ec5dbeaabc1b4a389645c1209
Iliya.Belichev <iliya.belichev@softwareag.com>
2020-05-11 22:03:38 +0300

    Adding Dockerfile + try/catch db initialization

--- 1.Python/1.DemoApp/footballapp/__init__.py
+++ 1.Python/1.DemoApp/footballapp/__init__.py
@@ -9,7 +9,11 @@ SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["FOOTBALL_APP_DB"] 
+
+try:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["FOOTBALL_APP_DB"] 
+except KeyError:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
 
 db = SQLAlchemy(app)
